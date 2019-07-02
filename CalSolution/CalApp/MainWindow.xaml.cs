f2ff0801ea898be91424b9847a9f09ae8441b47e
Luka Radovanovic <lukadlm97@gmail.com>
2019-07-02 14:39:49 +0200

    Implemented logic

--- CalSolution/CalApp/MainWindow.xaml.cs
+++ CalSolution/CalApp/MainWindow.xaml.cs
@@ -18,11 +18,315 @@ namespace CalApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public delegate decimal Operation(decimal x,decimal y);
     public partial class MainWindow : Window
     {
+        
+        string operation;
+        decimal firstNumberDec;
+        string secoundNumber = "";
+        decimal secoundNumberDec;
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "0";
+                secoundNumber += "0";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "0";
+            }
+            else
+            {
+                TextBoxResult.Text += "0";
+            }
+        }
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "1";
+                secoundNumber += "1";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "1";
+            }
+            else
+            {
+                TextBoxResult.Text += "1";
+            }
+
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "2";
+                secoundNumber += "2";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "2";
+            }
+            else
+            {
+                TextBoxResult.Text += "2";
+            }
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "3";
+                secoundNumber += "3";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "3";
+            }
+            else
+            {
+                TextBoxResult.Text += "3";
+            }
+        }
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "4";
+                secoundNumber += "4";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "4";
+            }
+            else
+            {
+                TextBoxResult.Text += "4";
+            }
+        }
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "5";
+                secoundNumber += "5";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "5";
+            }
+            else
+            {
+                TextBoxResult.Text += "5";
+            }
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "6";
+                secoundNumber += "6";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "6";
+            }
+            else
+            {
+                TextBoxResult.Text += "6";
+            }
+        }
+
+
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "7";
+                secoundNumber += "7";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "7";
+            }
+            else
+            {
+                TextBoxResult.Text += "7";
+            }
+        }
+
+        private void EightButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContainsOperation())
+            {
+                TextBoxResult.Text += "8";
+                secoundNumber += "8";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "8";
+            }
+            else
+            {
+                TextBoxResult.Text += "8";
+            }
+        }
+
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            if(ContainsOperation())
+            {
+                TextBoxResult.Text += "9";
+                secoundNumber += "9";
+                return;
+            }
+            if (IsEmpty())
+            {
+                TextBoxResult.Text = "9";
+            }
+            else
+            {
+                TextBoxResult.Text += "9";
+            }
+        }
+
+        private bool ContainsOperation()
+        {
+            return TextBoxResult.Text.Contains("+") || 
+                TextBoxResult.Text.Contains("*") || 
+                TextBoxResult.Text.Contains("/") || 
+                TextBoxResult.Text.Contains("-");
+        }
+
+        private bool IsEmpty()
+        {
+            return TextBoxResult == null || TextBoxResult.Text == "0";
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == null)
+            {
+                firstNumberDec = Convert.ToDecimal(TextBoxResult.Text);
+                operation = "+";
+                TextBoxResult.Text += operation;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == null)
+            {
+                firstNumberDec = Convert.ToDecimal(TextBoxResult.Text);
+                operation = "-";
+                TextBoxResult.Text += operation;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == null)
+            {
+                firstNumberDec = Convert.ToDecimal(TextBoxResult.Text);
+                operation = "*";
+                TextBoxResult.Text += operation;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Divided_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == null)
+            {
+                firstNumberDec = Convert.ToDecimal(TextBoxResult.Text);
+                operation = "/";
+                TextBoxResult.Text += operation;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            secoundNumberDec = Convert.ToDecimal(secoundNumber);
+            var result = 0.0m;
+            if (operation.Equals("+"))
+            {
+                Operation addDel = (x, y) => x + y;
+                result = ProcessData.Process(firstNumberDec, secoundNumberDec, addDel);
+                TextBoxResult.Text = Convert.ToString(result);
+                ClearVariable(result);
+                return;
+            }
+            if (operation.Equals("-"))
+            {
+                Operation addDel = (x, y) => x - y;
+                result = ProcessData.Process(firstNumberDec, secoundNumberDec, addDel);
+                TextBoxResult.Text = Convert.ToString(result);
+                ClearVariable(result);
+                return;
+            }
+            if (operation.Equals("*"))
+            {
+                Operation addDel = (x, y) => x * y;
+                result = ProcessData.Process(firstNumberDec, secoundNumberDec, addDel);
+                TextBoxResult.Text = Convert.ToString(result);
+                ClearVariable(result);
+                return;
+            }
+            if(operation.Equals("/"))
+            {
+                Operation addDel = (x, y) => x / y;
+                result = ProcessData.Process(firstNumberDec, secoundNumberDec, addDel);
+                TextBoxResult.Text = Convert.ToString(result);
+                ClearVariable(result);
+                return;
+            }
+
+        }
+
+        private void ClearVariable(decimal result)
+        {
+            firstNumberDec = result;
+            secoundNumber = null;
+            operation = null;
         }
     }
 }
---
+++ CalSolution/CalApp/ProcessData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalApp
+{
+    public class ProcessData
+    {
+        public static decimal Process(decimal num1,decimal num2,Operation del)
+        {
+            var result = del(num1, num2);
+            return result;
+        }
+    }
+}
